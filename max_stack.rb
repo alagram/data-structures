394c0673154a93f56d9ff478acfd97200d7192a5
Albert Agram <albert.agram@gmail.com>
2014-10-26 14:37:08 +0000

    add max_stack

--- max_stack.rb
+++ max_stack.rb
@@ -1,27 +1,38 @@
 require_relative 'linked_list'
+require_relative 'stack'
 
 # Implement a "max stack" - a stack that has a contant-time
 # "maximum" operations.
+def find_max(a, b)
+  a.to_f > b.to_i ? a : b
+end
 
 class MaxStack < Stack
   def initialize
+    super
+    @max = -Float::INFINITY
   end
 
   # Places +item+ on the top of the stack
   def push(item)
+    @max = find_max(@max, item)
+    new_max = [item, @max]
+    super(new_max)
   end
 
   # Removes the item on the top of the stack and returns it.
   # Raises an error if the stack is empty (called a "stack underflow")
   def pop
+    super[0]
   end
 
   # Return the item on the top of the stack without removing it
   def peek
+    super[0]
   end
 
-  # Return true if the stack is empty and false otherwise
   def empty?
+    super
   end
 
   # Return the number of items on the stack
@@ -31,5 +42,6 @@ class MaxStack < Stack
   # Returns the largest item on the stack
   # O(1) time
   def max
+    empty? ? -Float::INFINITY : @list.head.value[1]
   end
 end
