03177981378faf1dfd8d041ec8e54eb3f6dcbb72
Albert Agram <albert.agram@gmail.com>
2014-10-30 17:31:28 +0000

    Binary tree and Binary search tree implementation

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -9,4 +9,60 @@ require_relative 'binary_tree'
 #   empty?              O(1) time
 
 class BinarySearchTree < BinaryTree
+
+  def initialize(value)
+    super(value)
+  end
+
+  def insert(val)
+    if self.value < val
+      if (self.right)
+        self.right.insert(val)
+      else
+        self.right = BinarySearchTree.new(val)
+      end
+    else
+      if (self.left)
+        self.left.insert(val)
+      else
+        self.left = BinarySearchTree.new(val)
+      end
+    end
+  end
+
+  def include?(val)
+    if self.value == val
+      true
+    elsif self.value < val
+      if self.right
+        self.right.include?(val)
+      else
+        self.right == val
+      end
+    else
+      if (self.left)
+        self.left.include?(val)
+      else
+        self.left == val
+      end
+    end
+  end
+
+  def remove(val)
+    if self.value == val
+      self.value = nil
+    elsif self.value < val
+      if self.right
+        self.right.remove(val)
+      else
+        self.right = nil
+      end
+    else
+      if (self.left)
+        self.left.remove(val)
+      else
+        self.left = nil
+      end
+    end
+  end
 end
--- binary_tree.rb
+++ binary_tree.rb
@@ -1,13 +1,5 @@
 # Implement a basic (recursive) Binary Tree
-
-def BinaryTree(value)
-  case value
-  when BinaryTree
-    value
-  else
-    BinaryTree.new(value)
-  end
-end
+# Depth-first
 
 # For traversal, see
 # http://en.wikipedia.org/wiki/Tree_traversal#Implementations
@@ -15,21 +7,34 @@ end
 class BinaryTree
   attr_accessor :value, :left, :right
 
-  def initialize(value = nil)
+  def initialize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
   end
 
   def each(&block)
+    self.pre_order(&block)
   end
 
   # Implement pre-order traversal of the tree
   def pre_order(&block)
+    block.call(self.value)
+    self.left.pre_order(&block) if self.left
+    self.right.pre_order(&block) if self.right
   end
 
   # Implement in-order traversal of the tree
   def in_order(&block)
+    self.left.in_order(&block) if self.left
+    block.call(self.value)
+    self.right.in_order(&block) if self.right
   end
 
   # Implement post-order traversal of the tree
   def post_order(&block)
+    self.left.post_order(&block) if self.left
+    self.right.post_order(&block) if self.right
+    block.call(self.value)
   end
 end
