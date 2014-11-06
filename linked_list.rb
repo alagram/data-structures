3a0b399791af438c4622f28ba4de97b4b2ce4f8c
Albert Agram <albert.agram@gmail.com>
2014-11-06 12:14:30 +0000

    implement linked list append

--- linked_list.rb
+++ linked_list.rb
@@ -2,51 +2,71 @@ class LinkedList
   # By implementing LinkedList#each, we can include Enumerable
   include Enumerable
 
-  attr_reader :head, :tail
+  attr_accessor :head, :tail
 
   def initialize
-    @head = Node.new(nil)
+    @head = nil
     @tail = nil
   end
 
   # O(1) time
   # prepend/add node to front/biginning of LL
   def unshift(value)
-    new_node = Node(value)
+    new_node = Node.new(value)
 
-    new_node.next = @head
-    @head = new_node
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      new_node.next = @head
+      @head = new_node
+    end
 
     self
   end
 
   # O(1) time
-  # remove node from front/biginning of LL
+  # remove node from front/biginning of LL and return its value
   def shift
+    return nil if @head.nil?
+
     result = @head.value
-    @head = Node(@head.next)
+    @head = @head.next
+    result
+  end
+
+  # O(1) time
+  # remove node from front/beginning of LL and return it
+  def remove_node
+    return nil if @head.nil?
+
+    result = @head
+    @head = @head.next
     result
   end
 
   # O(1) time
   # add node to end of LL
-  # def push(value)
-  #   new_tail = Node(value)
-  #   @tail.next = new_tail
-  #   @tail = new_tail
-
-  #   self
-  # end
+  def append(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next = new_node
+      @tail = new_node
+    end
+  end
 
   # O(1) time
   def empty?
-    self.head.empty?
+    self.head.nil?
   end
 
   def each(&block)
     node = self.head
 
-    until node.empty?
+    until node.nil?
       block.call(node.value)
       node = node.next
     end
@@ -60,40 +80,31 @@ class LinkedList
     new_linked_list
   end
 
+  def reverse!
+    return if @head.nil?
+
+    @new_head = self.remove_node
+    @new_head.next = nil
+
+    until @head.nil?
+      node = self.remove_node
+      node.next = @new_head
+      @new_head = node
+    end
+
+    @head = @new_head
+  end
+
 end
 
-# This allows us to call Node(value) and is different
-# than the class "Node".  This is how Ruby methods
-# like Integer(value), Array(value), String(value), etc.
-# work.
-def Node(value)
-  case value
-  when Node
-    value
-  else
-    Node.new(value)
-  end
-end
 
 class Node
   include Enumerable
 
   attr_accessor :value, :next
 
-  def initialize(value = nil, next_node = nil)
+  def initialize(value = nil, new_node = nil)
     @value = value
-    @next  = next_node
-  end
-
-  # O(1) time
-  # Insert +value+ after this Node and return new Node
-  def insert_after(value)
-    node = Node(value)
-    node.next = self
-    node
-  end
-
-  def empty?
-    value.nil?
+    @next  = new_node
   end
 end
